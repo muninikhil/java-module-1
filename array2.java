63cca54265e7e448ff5780d3aa81e56a128095a1
NIKHIL desetti <nikhildesetti27@gmail.com>
2025-08-19 18:41:43 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class array2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println("You entered:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
