9979280957fc1d027afa60208283014fe769e41c
NIKHIL desetti <nikhildesetti27@gmail.com>
2025-08-20 19:35:04 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class greater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number");
+        int n1= sc.nextInt();
+        System.out.println("enter the second number");
+        int n2= sc.nextInt();
+        if(n1>n2)
+        {
+            System.out.println("the greater number is" + n1);
+        }
+        else
+        {
+            System.out.println("the greater number is" + n2);
+        }
+    }
+}
